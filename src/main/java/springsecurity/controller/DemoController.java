43329b98b7749890186fd74050b710eef7589544
gentlable <shingo.gt.1115@gmail.com>
2023-02-03 11:21:11 +0900

    first commit

---
+++ src/main/java/springsecurity/controller/DemoController.java
@@ -0,0 +1,13 @@
+package springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoController {
+	
+//	@GetMapping("/")
+//	public String getIndex() {
+//		return "index";
+//	}
+	
+}
